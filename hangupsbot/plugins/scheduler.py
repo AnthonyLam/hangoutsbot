391d97a0dd687a62c96ce97bbe003a2cc2fd58b6
Anthony Lam <thelamacmdr@gmail.com>
2015-10-28 20:53:34 -0700

    Add scheduler attempt

---
+++ hangupsbot/plugins/scheduler.py
@@ -0,0 +1,26 @@
+import plugins,re,calendar
+
+def _initialise(bot):
+    self.schedule = Schedule()
+
+
+@command.register(admin=False)
+def schedule(bot,event,date=None,item=None,*args):
+    yield from bot.coro_send_message(event.conf,_(calendar.HTMLCalendar().formatmonth(2015,11)))
+    
+    #yield from bot.coro_send_message(event.conv,_("<i>{} scheduled: {} @ {}</i>").format(event.user.full_name,))
+
+
+
+class Schedule():
+
+    def __init__(self):
+        self.date = None
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+
