d9d82b6c6bfe867b5090a8737da5106d739ebbee
Anthony Lam <thelamacmdr@gmail.com>
2015-10-30 17:30:24 -0700

    Testing regex

--- hangupsbot/plugins/urls.py
+++ hangupsbot/plugins/urls.py
@@ -5,12 +5,17 @@ def _initialise(bot):
     plugins.register_handler(_watch_for_url,"message")
     plugins.register_user_command(["url"])
 
-def url(bot,event,command,)
+def url(bot,event,command)
+    if command.startswith("clear"):
+        pass
+    else:
+        bot.memory.get_by_path(['url',event.conv_id])
 
 @asyncio.coroutine
 def _watch_for_url(bot,event,command):
     regex = "(http:|https:|www.|ftp:){1}.*"
     url = re.search(regex,event.text)
     
+    #bot.memory.set_by_path(['url',event.conv_id])
     event.coro_send_message(event.conv,"Got url: {}".format(url))
 
