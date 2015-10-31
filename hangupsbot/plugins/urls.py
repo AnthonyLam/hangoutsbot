292dedcccefb2392aed2f08f44c3cee7516a084c
Anthony Lam <thelamacmdr@gmail.com>
2015-10-30 17:43:58 -0700

    Change handler

--- hangupsbot/plugins/urls.py
+++ hangupsbot/plugins/urls.py
@@ -2,7 +2,7 @@ import plugins
 import re,asyncio
 
 def _initialise(bot):
-    plugins.register_handler(_watch_for_url,"message")
+    plugins.register_handler(_watch_for_url)
     plugins.register_user_command(["url"])
 
 def url(bot,event,command)
@@ -15,7 +15,8 @@ def url(bot,event,command)
 def _watch_for_url(bot,event,command):
     regex = "(http:|https:|www.|ftp:){1}.*"
     url = re.search(regex,event.text)
-    
+
     #bot.memory.set_by_path(['url',event.conv_id])
-    bot.coro_send_message(event.conv,"Got url: {}".format(url))
+    if url:
+        bot.coro_send_message(event.conv,"Got url: {}".format(url.group(0)))
 
